98c413f2a5312e8c31528bfb56dd0acaf4cf3585
Ryan Manchester <ryanm17@gmail.com>
2021-09-24 13:07:20 -0400

    creates initial search form

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react';
+
  const RepositoriesList: React.FC = () => {
-  return <div>
-   <form>
-    <input />
+   const [term, setTerm] = useState('');
+   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+     event.preventDefault();
+     alert('Sumbitted!')
+   }
+
+  return (<div>
+   <form onSubmit={onSubmit}>
+    <input value={term} onChange={(e) => setTerm(e.target.value)} />
     <button>Search</button>
    </form>
   </div>
+)
 }
 
 export default RepositoriesList
